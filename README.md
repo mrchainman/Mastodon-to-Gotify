f1ca9f0fa7e264db1260c9d2a87c04bf262f0aad
Mr Chainman <mr.chainman@0box.eu>
2020-03-23 13:01:01 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mastodon to Gotify
+This Programm fetches notifications from Mastodon and pushes them to a Gotify server.
--- first_run.py
+++ first_run.py
@@ -2,6 +2,6 @@
 from settings import *
 Mastodon.create_app(
      app_name,
-     api_base_url = 'https://mastodon.social',
+     api_base_url = instance_url,
      to_file = f"{app_name}_cred.secret"
 )
